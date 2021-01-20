a00145f86fbc1a94744e61f18593470b3fd25ba6
yyjazsf <admin@yingyj.com>
2021-01-20 14:13:30 +0800

    chroe: rm useless code

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 // const isProduction = process.env["NODE_ENV"] === "production"
-const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   pluginOptions: {
@@ -21,15 +20,6 @@ module.exports = {
     }
   },
   configureWebpack: {
-    // target: "electron-renderer",
     externals: { "agora-electron-sdk": "commonjs2 agora-electron-sdk" },
-    plugins: [
-      // new CopyPlugin([
-      //   {
-      //     from: "node_modules/agora-electron-sdk",
-      //     to: "node_modules/agora-electron-sdk"
-      //   }
-      // ])
-    ]
   }
 };
